c90826c8c0e97e3e88b026c45c984120619f2957
vivincrichard <vivincrichard@gmail.com>
2024-11-30 11:44:02 +0530

    Formik

--- src/Components/Hospitals/CreateUpdateHospital.tsx
+++ src/Components/Hospitals/CreateUpdateHospital.tsx
@@ -1,37 +1,76 @@
-import { Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import { useCreateHospital } from "./query/HospitalQuery";
-import { IHospitalPayload } from "./service/HospitalService"
-import * as Yup from 'yup';
-interface IProps {
-    hospital: IHospitalPayload;
-}
+import { IHospitalPayload } from "./service/HospitalService";
+import * as Yup from "yup";
+import { useRef } from "react";
+// interface IProps {
+//   hospital: IHospitalPayload;
+// }
 
-function CreateUpdateHospital(props : IProps) {
+function CreateUpdateHospital() {
+  const formikRef: any = useRef();
 
+  const { mutateAsync: createHospital } = useCreateHospital();
 
-    const {mutateAsync: createHospital} = useCreateHospital()
+  const initialData = {
+    id: "",
+    hospitalName: "",
+    registrationNo: "",
+  };
 
-    const initialData = {
-        id:'',
-        hospitalNme:'',
-        registrationNo:'',
-    }
-
-    const validationSchema = Yup.object({
-        id:
-    })
-
+  const validationSchema = Yup.object({
+    id: Yup.string().required("Required").max(10, "Less than 10 Characters"),
+    hospitalName: Yup.string().required("Required"),
+    registrationNo: Yup.number().required("Required"),
+  });
 
   return (
     <>
-        <Formik
-            initialValues={initialData}
-            validationSchema={validationSchema}
-        >
-
-        </Formik>
+      <Formik
+        innerRef={formikRef}
+        initialValues={initialData}
+        validationSchema={validationSchema}
+        enableReinitialize={true}
+        onSubmit={(values) => {
+          console.log("values", values);
+        }}
+      >
+        {({ values }) => (
+          <Form>
+            <div className="from-group">
+              <label htmlFor="name" className="form-label">
+                Hospital Name
+                <span className="text-danger">*</span>
+              </label>
+              <Field
+                type="text"
+                name="name"
+                id="name"
+                values={values?.hospitalName}
+                className="form-control"
+              />
+              <label htmlFor="regNo">Registration No</label>
+              <Field
+                type="number"
+                name="regNo"
+                id="regNo"
+                values={values?.registrationNo}
+                className="form-control"
+              />
+              <div className="d-flex justify-content-end mt-2 gap-2">
+                <button className="btn btn-light" type="button">
+                  Clear
+                </button>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </>
-  )
+  );
 }
 
-export default CreateUpdateHospital
+export default CreateUpdateHospital;
