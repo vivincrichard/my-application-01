0219088a6f7387ac57d366937606f7f16db57ff4
vivincrichard <vivincrichard@gmail.com>
2025-01-17 08:38:39 +0530

    utils change

--- src/Components/Utils.ts
+++ src/Components/Utils.ts
@@ -1,10 +1,13 @@
-export const capitalFirstLetters = (input: any) => {
-  if (!input) return ""; // Handle empty input
-  return input
-    .toLowerCase()
-    .split(" ") // Split the string into words
-    .map(
-      (word: any) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    ) // Capitalize each word
-    .join(" "); // Join the words back into a single string
+export const capitalFirstLetters = (value: any) => {
+  if (value) {
+    return value
+      .toLowerCase()
+      .split(" ") // Split the string into words
+      .map(
+        (word: any) =>
+          word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      ) // Capitalize each word
+      .join(" "); // Join the words back into a single string
+  }
+  return value;
 };
