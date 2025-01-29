c5980189c54498dca36b3ab28ac1fd4ab877f804
vivincrichard <vivincrichard@gmail.com>
2025-01-29 22:46:37 +0530

    added validation to name in staff

--- src/Components/Staff/Index.tsx
+++ src/Components/Staff/Index.tsx
@@ -5,6 +5,7 @@ import { genderOptions, IStaff } from "./staffService";
 import { Controller, useForm } from "react-hook-form";
 import { capitalFirstLetters } from "../Utils";
 import ShiftType, { shiftEnumType } from "./StaffHelper";
+import { BiPlusCircle } from "react-icons/bi";
 
 type OptionType = { value: string | any; label: string };
 
@@ -12,6 +13,9 @@ function Staff() {
   const { data: staffList } = useStaffList();
   const { mutateAsync: createStaff } = useStaffCreate();
   const { data: departmentData } = useDepartments();
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const toggleVisibility = () => setToggle(!toggle);
 
   const {
     register,
@@ -30,6 +34,7 @@ function Staff() {
       email: null,
       shift: null,
     },
+    // resolver: YupResolver()
   });
 
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -89,6 +94,7 @@ function Staff() {
 
     createStaff(newStaff);
     clearValues();
+    toggleVisibility();
   };
 
   const watchValues = watch();
@@ -99,182 +105,201 @@ function Staff() {
 
   return (
     <div className="m-3">
-      <h5>Create Staff</h5>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="text-end mb-1">
-          <button
-            type="button"
-            className="btn btn-outline-light text-dark mx-1"
-            onClick={clearValues}
-          >
-            Clear
-          </button>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <div className="row">
-          <div className="form-group col-12 col-sm-6 col-md-3">
-            <div className="row">
-              <label
-                htmlFor="name"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Name
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  {...register("name", { required: "Required" })}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="form-group col-12 col-sm-6 col-md-3">
-            <div className="row">
-              <label
-                htmlFor="gender"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Gender
-              </label>
-              <div className="col-sm-8 m-0 p-0">
-                <Controller
-                  name="gender"
-                  control={control}
-                  render={({ field }) => (
-                    <Select
-                      {...field}
-                      options={genderOptions as OptionType[]}
-                      value={genderOptions.find(
-                        (opt) => opt.value === field.value
-                      )}
-                      onChange={(opt) => field.onChange(opt?.value)}
-                    />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="form-group col-12 col-sm-6 col-md-3">
-            <div className="row">
-              <label
-                htmlFor="age"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Age
-              </label>
-              <div className="col-8 m-0 p-0">
-                <input
-                  type="number"
-                  className="form-control"
-                  id="age"
-                  {...register("age")}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="form-group col-12 col-sm-6 col-md-3">
-            <div className="row">
-              <label
-                htmlFor="phoneNumber"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Phone Number
-              </label>
-              <div className="col-8 m-0 p-0">
-                <input
-                  type="number"
-                  id="phoneNumber"
-                  className="form-control"
-                  {...register("phoneNumber")}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="form-group col-12 col-sm-6 col-md-3 mt-1">
-            <div className="row">
-              <label
-                htmlFor="email"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Email
-              </label>
-              <div className="col-8 m-0 p-0">
-                <input
-                  type="email"
-                  id="email"
-                  className="form-control"
-                  {...register("email")}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="form-group col-12 col-sm-6 col-md-3 mt-1">
-            <div className="row">
-              <label
-                htmlFor="role"
-                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
-              >
-                Role
-              </label>
-              <div className="col-8 m-0 p-0">
-                <Controller
-                  name="role"
-                  control={control}
-                  render={({ field }) => (
-                    <Select
-                      {...field}
-                      options={departmentOptions}
-                      value={departmentOptions?.find(
-                        (opt) => opt.value === field.value
-                      )}
-                      onChange={(opt) => field.onChange(opt?.value)}
-                    />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="form-group d-flex col-12 col-sm-6 col-md-3 mt-1 p-0">
-            <label
-              htmlFor="shift"
-              className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+      <h5 onClick={toggleVisibility}>
+        Create Staff <BiPlusCircle className="text-primary" />
+      </h5>
+      {toggle && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="text-end mb-1">
+            <button
+              type="button"
+              className="btn btn-outline-light text-dark mx-1"
+              onClick={clearValues}
             >
-              Shift
-            </label>
-            <div className="d-flex col-8 gap-3">
-              <div className="form-check">
-                <input
-                  id="day_shift"
-                  type="radio"
-                  value={1} // Numeric value
-                  className="form-check-input"
-                  {...register("shift")} // Ensures value is a number
-                />
-                <label htmlFor="day_shift" className="form-check-label">
-                  Day Shift
+              Clear
+            </button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <div className="row">
+            <div className="form-group col-12 col-sm-6 col-md-3">
+              <div className="row">
+                <label
+                  htmlFor="name"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Name
                 </label>
+                <div className="col-sm-8">
+                  <input
+                    type="text"
+                    id="name"
+                    className={`form-control ${
+                      errors.name ? "is-invalid" : ""
+                    }`}
+                    {...register("name", {
+                      required: "Required",
+                      pattern: {
+                        value: /^[A-Za-z\s]+$/,
+                        message: "Only alphabets are allowed",
+                      },
+                      minLength: {
+                        value: 5,
+                        message: 'Not exceed than 5'
+                      }
+                    })}
+                  />
+                  {errors.name && (
+                    <small className="text-danger">{errors.name.message}</small>
+                  )}
+                </div>
               </div>
-              <div className="form-check">
-                <input
-                  id="night_shift"
-                  type="radio"
-                  value={2} // Numeric value
-                  className="form-check-input"
-                  {...register("shift")} // Ensures value is a number
-                />
-                <label htmlFor="night_shift" className="form-check-label">
-                  Night Shift
+            </div>
+
+            <div className="form-group col-12 col-sm-6 col-md-3">
+              <div className="row">
+                <label
+                  htmlFor="gender"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Gender
                 </label>
+                <div className="col-sm-8 m-0 p-0">
+                  <Controller
+                    name="gender"
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        options={genderOptions as OptionType[]}
+                        value={genderOptions.find(
+                          (opt) => opt.value === field.value
+                        )}
+                        onChange={(opt) => field.onChange(opt?.value)}
+                      />
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group col-12 col-sm-6 col-md-3">
+              <div className="row">
+                <label
+                  htmlFor="age"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Age
+                </label>
+                <div className="col-8 m-0 p-0">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="age"
+                    {...register("age")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group col-12 col-sm-6 col-md-3">
+              <div className="row">
+                <label
+                  htmlFor="phoneNumber"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Phone Number
+                </label>
+                <div className="col-8 m-0 p-0">
+                  <input
+                    type="number"
+                    id="phoneNumber"
+                    className="form-control"
+                    {...register("phoneNumber")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group col-12 col-sm-6 col-md-3 mt-1">
+              <div className="row">
+                <label
+                  htmlFor="email"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Email
+                </label>
+                <div className="col-8 m-0 p-0">
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    {...register("email")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group col-12 col-sm-6 col-md-3 mt-1">
+              <div className="row">
+                <label
+                  htmlFor="role"
+                  className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+                >
+                  Role
+                </label>
+                <div className="col-8 m-0 p-0">
+                  <Controller
+                    name="role"
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        options={departmentOptions}
+                        value={departmentOptions?.find(
+                          (opt) => opt.value === field.value
+                        )}
+                        onChange={(opt) => field.onChange(opt?.value)}
+                      />
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group d-flex col-12 col-sm-6 col-md-3 mt-1 p-0">
+              <label
+                htmlFor="shift"
+                className="col-4 form-label d-flex justify-content-center fw-bold m-0 p-0"
+              >
+                Shift
+              </label>
+              <div className="d-flex col-8 gap-3">
+                <div className="form-check">
+                  <input
+                    id="day_shift"
+                    type="radio"
+                    value={1} // Numeric value
+                    className="form-check-input"
+                    {...register("shift")} // Ensures value is a number
+                  />
+                  <label htmlFor="day_shift" className="form-check-label">
+                    Day Shift
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    id="night_shift"
+                    type="radio"
+                    value={2} // Numeric value
+                    className="form-check-input"
+                    {...register("shift")} // Ensures value is a number
+                  />
+                  <label htmlFor="night_shift" className="form-check-label">
+                    Night Shift
+                  </label>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      )}
 
       <h5>Search Staff</h5>
       <input
