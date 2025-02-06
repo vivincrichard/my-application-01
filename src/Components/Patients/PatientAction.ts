c3df26c3b986053b549728aef6a0dda8e3dd577d
vivincrichard <vivincrichard@gmail.com>
2025-02-06 22:52:23 +0530

    added redux

--- src/Components/Patients/PatientAction.ts
+++ src/Components/Patients/PatientAction.ts
@@ -1,7 +1,10 @@
 // Action Creator
 
-import { SET_PATIENTS, SET_PATIENTS_LOADING } from "./types";
-
+import { ThunkAction } from "redux-thunk";
+import { IPatient, SET_PATIENTS, SET_PATIENTS_LOADING } from "./types";
+import { Action } from "redux";
+import axios, { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 // Set patient list
 export const setPatients = (patients: any) => ({
@@ -14,3 +17,13 @@ export const setPatientsLoading = (isLoading: boolean) => ({
   type: SET_PATIENTS_LOADING,
   payload: isLoading,
 });
+
+export const createPatient =
+  (patientData: IPatient): ThunkAction<void, null, null, Action<string>> =>
+  async (dispatch) => {
+    try{
+      dispatch(setPatientsLoading(true));
+      const response = await axios.post(`http://localhost:4000/patients`,patientData);
+      dispatch(setPatientsLoading(false))
+    }catch{}
+  };
