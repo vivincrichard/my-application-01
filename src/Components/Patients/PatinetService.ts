7a36192896d74bc7bc06aa66bd4cc8248a865966
vivincrichard <vivincrichard@gmail.com>
2025-02-21 20:33:16 +0530

    added query key and service

--- src/Components/Patients/PatinetService.ts
+++ src/Components/Patients/PatinetService.ts
@@ -1,9 +1,26 @@
-import axios from "axios"
-
-
+import axios from "axios";
+import { IPatient } from "./types";
 
 export class Patient {
-    static create = async() => {
-        const response = await axios.post(`http://localhost:4000/patients`);
-    }
+  static create = async (data: IPatient) => {
+    const response = await axios.post(`http://localhost:4000/patients`);
+    return response.data;
+  };
+
+  static update = async (id: string,data: IPatient) => {
+    const response = await axios.patch(`http://localhost:4000/patients/${id}`,data);
+    return response.data;
+  }
+
+  static delete = async (id:string) => {
+    
+  }
+
+  static getById = async (id: string) => {
+    const response = await axios.get(`http://localhost:4000/patients/${id}`);
+    return response.data;
+  };
+
+
+
 }
